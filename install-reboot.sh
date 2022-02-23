30d975ffc4d5270865afdd2f3cd40fa1d8a30aa0
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-23 19:40:00 +0100

    Update install-reboot.sh

--- install-reboot.sh
+++ install-reboot.sh
@@ -3,4 +3,5 @@ mkdir ~/.config-dar
 cd ~/.config-dar
 wget https://raw.githubusercontent.com/Jontes-Tech/dont-actually-reboot/main/dont-actually-reboot.sh
 chmod +x $HOME/.config-dar/*
-echo "Please add this line to your shellrc file: alias rebot='bash $HOME/.config-dar/dont-actually-reboot.sh'
+echo 'bash $HOME/.config-dar/dont-actually-reboot.sh' >> ~/.bashrc
+echo 'bash $HOME/.config-dar/dont-actually-reboot.sh' >> ~/.zshrc
