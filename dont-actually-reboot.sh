6174792be86c6346b221aba8286f22e6023b760d
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-25 09:00:36 +0100

    Update dont-actually-reboot.sh

--- dont-actually-reboot.sh
+++ dont-actually-reboot.sh
@@ -1,8 +1,8 @@
 while true; do
-    read -p "Do you wish to reboot?!" yn
+    read -p "Do you wish to reboot?! y or n " yn
     case $yn in
         [Yy]* ) sudo reboot;;
         [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo "Please answer y or n";;
     esac
 done
