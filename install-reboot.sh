47c976e48447047dede8297fd574250296ea524f
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-25 08:35:39 +0100

    Update install-reboot.sh

--- install-reboot.sh
+++ install-reboot.sh
@@ -3,4 +3,4 @@ mkdir ~/.config-dar
 cd ~/.config-dar
 wget https://raw.githubusercontent.com/Jontes-Tech/dont-actually-reboot/main/dont-actually-reboot.sh
 chmod +x $HOME/.config-dar/*
-cp dont-actually-reboot.sh /usr/bin/
+sudo cp dont-actually-reboot.sh /usr/bin/
