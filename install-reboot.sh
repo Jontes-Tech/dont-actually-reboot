a4fe311416e259991b0dec869a56786e003525ab
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-25 15:11:11 +0100

    Update install-reboot.sh

--- install-reboot.sh
+++ install-reboot.sh
@@ -3,7 +3,7 @@ mkdir ~/.config-dar
 cd ~/.config-dar
 wget -q https://raw.githubusercontent.com/Jontes-Tech/dont-actually-reboot/main/dont-actually-reboot.sh
 wget -q -O installedversion.txt https://raw.githubusercontent.com/Jontes-Tech/dont-actually-reboot/main/newestversion.txt
-if [ -f "/usr/share/actually-reboot" ]; then
+if [ -f "/usr/sbin/actually-reboot" ]; then
     echo "This is not your first time running the script."
 else 
     sudo mv /usr/sbin/reboot /usr/sbin/actually-reboot
