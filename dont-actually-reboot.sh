b3ccc2369a07ce3472032d5463f2a1f3a1bb4b20
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-25 09:33:35 +0100

    Update dont-actually-reboot.sh

--- dont-actually-reboot.sh
+++ dont-actually-reboot.sh
@@ -1,4 +1,12 @@
 while true; do
+    read -p "Do you wish to check for updates of this software? y or n " yn
+    case $yn in
+        [Yy]* ) bash -c "$(curl -fsSL https://go.jontes.page/udar.sh)";;
+        [Nn]* ) echo "No problem, let's move on.";;
+        * ) echo "Please answer y or n";;
+    esac
+done
+while true; do
     read -p "Do you wish to reboot?! y or n " yn
     case $yn in
         [Yy]* ) sudo reboot;;
