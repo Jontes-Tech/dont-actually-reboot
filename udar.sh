1c380d598bbae17ff05ca914f40c04cecfb5867b
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-25 14:18:53 +0100

    Update udar.sh

--- udar.sh
+++ udar.sh
@@ -1,3 +1,15 @@
-echo "You are on the latest version"
-sleep 5
-sudo rebot
+wget -q -o ~/.config-dar/newestversion.txt https://jontes.page/darversion.txt
+echo "Your system runs:"
+cat ~/.config-dar/installedversion.txt
+echo "---"
+echo "And the newest version is:"
+cat ~/.config-dar/newestversion.txt
+while true; do
+    read -p "Do you want to upgrade? y/n " yn
+    case $yn in
+        [Yy]* ) bash -c "$(curl -fsSL https://go.jontes.page/dar.sh)";;
+        [Nn]* ) break;;
+        * ) echo "Please answer y or n";;
+    esac
+done
+sudo reboot
