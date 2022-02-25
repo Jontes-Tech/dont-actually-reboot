2fd659200237321b71d9e26cbd086806e407ce71
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-02-25 10:02:19 +0100

    Update dont-actually-reboot.sh

--- dont-actually-reboot.sh
+++ dont-actually-reboot.sh
@@ -1,10 +1,4 @@
-if echo $* | grep -e "--flag" -q
-then
-  noupdate=1
-else
-  noupdate=0
-fi
-while [ "$noupdate" -eq 0 ]; do
+while true; do
     read -p "Do you wish to check for updates of this software? y or n " yn
     case $yn in
         [Yy]* ) bash -c "$(curl -fsSL https://go.jontes.page/udar.sh)";;
